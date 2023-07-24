0d553715249515a03d61f572053f882e4b8f7075
DeRhode <Kuubeyangndaarhoderich@yahoo.com>
2023-07-24 23:10:59 +0000

    Reviewing size_get.c

--- size_get.c
+++ size_get.c
@@ -10,18 +10,26 @@
 
 int get_size(const char *format, int *i)
 {
-	int curr_i = *i + 1;
+	int c_i = *i + 1;
 	int size = 0;
 
-	if (format[curr_i] == 'l')
+	if (format[c_i] == 'l')
+	{
 		size = S_LONG;
-	else if (format[curr_i] == 'h')
+	}
+	else if (format[c_i] == 'h')
+	{
 		size = S_SHORT;
+	}
 
 	if (size == 0)
-		*i = curr_i - 1;
+	{
+		*i = c_i - 1;
+	}
 	else
-		*i = curr_i;
+	{
+		*i = c_i;
+	}
 
 	return (size);
 }
