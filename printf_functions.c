e1a8d51b705e70519510e074edb1b85da0c64cd0
SalvatorePaul <kehindepaul105@gmail.com>
2023-07-24 11:16:42 -0700

    size and functions

--- printf_functions.c
+++ printf_functions.c
@@ -4,11 +4,11 @@
 
 /**
  * Authors: Paul and Rhoderick
- * print_char - Prints a char
+ * print_char - Prints char
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
- * @width: Width
+ * @width: Get width
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
@@ -22,7 +22,7 @@ int print_char(va_list types, char buffer[],
 }
 /************************* PRINT A STRING *************************/
 /**
- * print_string - Prints a string
+ * print_string - Prints string
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
@@ -31,6 +31,7 @@ int print_char(va_list types, char buffer[],
  * @size: Size specifier
  * Return: Number of chars printed
  */
+
 int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
